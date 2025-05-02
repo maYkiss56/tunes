ce5175a22c27afbd686dcfbf0276e798a1636366
Michael Grischenko <mikigym19@gmail.com>
2025-05-02 12:42:30 +0300

    replace log or fmt on logger

--- internal/server/http.go
+++ internal/server/http.go
@@ -7,15 +7,17 @@ import (
 	"net/http"
 
 	"github.com/maYkiss56/tunes/internal/config"
+	"github.com/maYkiss56/tunes/internal/logger"
 )
 
 type HTTPServer struct {
 	server   *http.Server
 	listener net.Listener
 	cfg      *config.Config
+	logger   *logger.Logger
 }
 
-func NewHTTPServer(cfg *config.Config) (*HTTPServer, error) {
+func NewHTTPServer(cfg *config.Config, logger *logger.Logger) (*HTTPServer, error) {
 	addr := net.JoinHostPort(cfg.HTTP.Host, cfg.HTTP.Port)
 
 	listener, err := net.Listen(cfg.HTTP.Network, addr)
@@ -35,17 +37,24 @@ func NewHTTPServer(cfg *config.Config) (*HTTPServer, error) {
 		server:   server,
 		listener: listener,
 		cfg:      cfg,
+		logger:   logger,
 	}, nil
 }
 
 func (s *HTTPServer) Start(serverErr chan<- error) {
-	fmt.Printf("Starting server at http://%s\n", s.listener.Addr().String())
+	s.logger.Info(
+		"Starting HTTP server",
+		"address",
+		s.listener.Addr().String(),
+		"timeout",
+		s.cfg.HTTP.ReadTimeout,
+	)
 	if err := s.server.Serve(s.listener); err != nil && err != http.ErrServerClosed {
 		serverErr <- fmt.Errorf("server error: %w", err)
 	}
 }
 
 func (s *HTTPServer) Shutdown(ctx context.Context) error {
-	fmt.Println("Shutting down server...")
+	s.logger.Info("Shutting down HTTP server")
 	return s.server.Shutdown(ctx)
 }
