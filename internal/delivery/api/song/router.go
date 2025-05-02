3b9b6bbcfab8eeb109fa9a2dd7f14c8b9eba6bf8
Michael Grischenko <mikigym19@gmail.com>
2025-05-02 17:23:25 +0300

    add routes for songs handler

---
+++ internal/delivery/api/song/router.go
@@ -0,0 +1,17 @@
+package song
+
+import (
+	"github.com/go-chi/chi/v5"
+)
+
+func RegisterRoutes(r chi.Router, handler *Handler) {
+	r.Route("/songs", func(r chi.Router) {
+		r.Post("/", handler.CreateSong)
+		r.Get("/", handler.GetAllSongs)
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", handler.GetSongByID)
+			r.Put("/", handler.UpdateSong)
+			r.Delete("/", handler.DeleteSong)
+		})
+	})
+}
