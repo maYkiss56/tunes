72e96b2b4d33d736efbd8a26df6dbe9ab85f05c7
Michael Grischenko <mikigym19@gmail.com>
2025-05-02 12:42:14 +0300

    replace log or fmt on logger

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,41 +5,58 @@ import (
 	"fmt"
 
 	"github.com/maYkiss56/tunes/internal/config"
+	"github.com/maYkiss56/tunes/internal/logger"
 	"github.com/maYkiss56/tunes/internal/server"
 )
 
 type App struct {
 	cfg        *config.Config
 	httpServer *server.HTTPServer
+	logger     *logger.Logger
 }
 
-func New(cfg *config.Config) (*App, error) {
-	httpServer, err := server.NewHTTPServer(cfg)
+func New(cfg *config.Config, logger *logger.Logger) (*App, error) {
+	httpServer, err := server.NewHTTPServer(cfg, logger)
 	if err != nil {
-		return nil, err
+		logger.Error("Failed to create HTTP server", "error", err)
+		return nil, fmt.Errorf("http server creation failed: %w", err)
 	}
 
 	return &App{
 		cfg:        cfg,
+		logger:     logger,
 		httpServer: httpServer,
 	}, nil
 }
 
 func (a *App) Run(ctx context.Context) error {
+	defer a.logger.Shutdown()
+
+	a.logger.Info(
+		"Starting application",
+	)
+
 	serverErr := make(chan error, 1)
 
 	go a.httpServer.Start(serverErr)
 
 	select {
 	case err := <-serverErr:
+		a.logger.Error("Server stopped with error", "error", err)
 		return err
 	case <-ctx.Done():
-		fmt.Println("Context canceled, shutting down")
+		a.logger.Info("Received shutdown signal")
 		shutdownCtx, cancel := context.WithTimeout(
 			context.Background(),
 			a.cfg.HTTP.GracefullTimeout,
 		)
 		defer cancel()
-		return a.httpServer.Shutdown(shutdownCtx)
+
+		if err := a.httpServer.Shutdown(shutdownCtx); err != nil {
+			a.logger.Error("Greceful shutdown failed", "error", err)
+			return fmt.Errorf("shutdown failed: %w", err)
+		}
+		a.logger.Info("Application stopped gracefully")
+		return nil
 	}
 }
