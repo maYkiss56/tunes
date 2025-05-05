1e5d29741bdeabeebbe3036b855eb4f067db6ff6
Michael Grischenko <mikigym19@gmail.com>
2025-05-05 16:57:09 +0300

    add router for artist

--- internal/delivery/api/router.go
+++ internal/delivery/api/router.go
@@ -3,18 +3,27 @@ package api
 import (
 	"github.com/go-chi/chi/v5"
 
+	artistHandler "github.com/maYkiss56/tunes/internal/delivery/api/artist"
 	songHandler "github.com/maYkiss56/tunes/internal/delivery/api/song"
 	"github.com/maYkiss56/tunes/internal/logger"
 	"github.com/maYkiss56/tunes/internal/middleware"
 )
 
-func NewRouter(song *songHandler.Handler, logger *logger.Logger) chi.Router {
+func NewRouter(
+	song *songHandler.Handler,
+	artist *artistHandler.Handler,
+	logger *logger.Logger,
+) chi.Router {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RecoverMiddleware(logger))
 	songRouter := chi.NewRouter()
 	songHandler.RegisterRoutes(songRouter, song)
-	r.Mount("/api", songRouter)
+	r.Mount("/api/songs", songRouter)
+
+	artistRouter := chi.NewRouter()
+	artistHandler.RegisterRoutes(artistRouter, artist)
+	r.Mount("/api/artists", artistRouter)
 
 	return r
 }
