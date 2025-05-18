7eb63190f600f0dfc2a90b19e8850e54b35d9f3a
Michael Grischenko <mikigym19@gmail.com>
2025-05-18 14:07:18 +0300

    add fabric for song model

--- internal/domain/song/song.go
+++ internal/domain/song/song.go
@@ -15,3 +15,23 @@ type Song struct {
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 }
+
+func NewSong(
+	title string,
+	fullTitle string,
+	imageURL string,
+	releaseData time.Time,
+	// artistID int,
+	// albumID int,
+) (*Song, error) {
+	return &Song{
+		Title:       title,
+		FullTitle:   fullTitle,
+		ImageURL:    imageURL,
+		ReleaseDate: releaseData,
+		// ArtistID:    artistID,
+		// AlbumID:     albumID,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}, nil
+}
