a7e8e08fcc4b6417cd51e6bb0b4058ee7bc4124a
Michael Grischenko <mikigym19@gmail.com>
2025-05-28 12:58:17 +0300

    update model for genre

--- internal/domain/genre/genre.go
+++ internal/domain/genre/genre.go
@@ -1,10 +1,14 @@
 package genre
 
 type Genre struct {
-	ID    int
-	Title string
+	ID       int
+	Title    string
+	ImageURL string
 }
 
-func NewGenre(title string) (*Genre, error) {
-	return &Genre{Title: title}, nil
+func NewGenre(title string, imageURL string) (*Genre, error) {
+	return &Genre{
+		Title:    title,
+		ImageURL: imageURL,
+	}, nil
 }
