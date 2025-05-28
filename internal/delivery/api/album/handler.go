5346a865845eb0d8f1518e63313d5889253f9793
Michael Grischenko <mikigym19@gmail.com>
2025-05-28 12:52:58 +0300

    update CreateAlbum and UpdateAlbum method

--- internal/delivery/api/album/handler.go
+++ internal/delivery/api/album/handler.go
@@ -2,7 +2,6 @@ package album
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
 	"strconv"
 
@@ -35,19 +34,44 @@ func NewHandler(service AlbumService, logger *logger.Logger) *Handler {
 }
 
 func (h *Handler) CreateAlbum(w http.ResponseWriter, r *http.Request) {
-	var req dto.CreateAlbumRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		h.logger.Error("invalid request body", "error", err)
-		utilites.RenderError(w, r, http.StatusBadRequest, "invalid request body")
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		h.logger.Error("failed to parse mulipart form", "error", err)
+		utilites.RenderError(w, r, http.StatusBadRequest, "failed to parse form")
 		return
 	}
 
-	if err := req.Validate(); err != nil {
-		utilites.RenderError(w, r, http.StatusBadRequest, err.Error())
+	title := r.FormValue("title")
+	file, header, err := r.FormFile("image")
+	if err != nil {
+		h.logger.Error("failed to get image file", "error", err)
+		utilites.RenderError(w, r, http.StatusBadRequest, "failed to get image")
+		return
+	}
+	artistIDStr := r.FormValue("artist_id")
+	artistID, err := strconv.Atoi(artistIDStr)
+	if err != nil {
+		h.logger.Error("failed to convert artist_ID string -> int", "error", err)
+		utilites.RenderError(w, r, http.StatusBadRequest, "failed to get artist_id")
 		return
 	}
 
-	newAlbum, err := domain.NewAlbum(req.Title, req.ImageURL, req.ArtistID)
+	defer func() {
+		if file != nil {
+			file.Close()
+		}
+	}()
+
+	var imagePath string
+	if file != nil {
+		imagePath, err = utilites.SaveImage(file, header, "static/uploads/albums")
+		if err != nil {
+			h.logger.Error("failed to save image", "error", err)
+			utilites.RenderError(w, r, http.StatusInternalServerError, "failed to save image")
+			return
+		}
+	}
+
+	newAlbum, err := domain.NewAlbum(title, imagePath, artistID)
 	if err != nil {
 		h.logger.Error("invalid input album", "error", err)
 		utilites.RenderError(w, r, http.StatusBadRequest, err.Error())
@@ -60,7 +84,10 @@ func (h *Handler) CreateAlbum(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	utilites.RenderJSON(w, r, http.StatusCreated, dto.ToResponse(*newAlbum))
+	res := dto.ToResponse(*newAlbum)
+	res.ImageURL = utilites.GetImageURL(imagePath)
+
+	utilites.RenderJSON(w, r, http.StatusCreated, res)
 }
 
 func (h *Handler) GetAllAlbums(w http.ResponseWriter, r *http.Request) {
@@ -89,7 +116,6 @@ func (h *Handler) GetAlbumByID(w http.ResponseWriter, r *http.Request) {
 
 	a, err := h.service.GetAlbumByID(r.Context(), id)
 	if err != nil {
-		//TODO: err not found
 		h.logger.Error("failed to get album by id", "error", err)
 		utilites.RenderError(w, r, http.StatusInternalServerError, "failed to get album by id")
 		return
@@ -106,26 +132,57 @@ func (h *Handler) UpdateAlbum(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var req dto.UpdateAlbumRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		h.logger.Error("invalid request body", "error", err)
-		utilites.RenderError(w, r, http.StatusBadRequest, "invalid request body")
+	// max 10 MB
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		h.logger.Error("failed to parse form", "error", err)
+		utilites.RenderError(w, r, http.StatusBadRequest, "failed to parse form")
 		return
 	}
 
-	if err := req.Validate(); err != nil {
-		utilites.RenderError(w, r, http.StatusBadRequest, err.Error())
-		return
+	req := dto.UpdateAlbumRequest{}
+
+	if title := r.FormValue("title"); title != "" {
+		req.Title = &title
+	}
+
+	file, header, err := r.FormFile("image")
+	if err == nil {
+		defer file.Close()
+
+		imagePath, err := utilites.SaveImage(file, header, "static/uploads/albums")
+		if err != nil {
+			h.logger.Error("failed to save image", "error", err)
+			utilites.RenderError(w, r, http.StatusInternalServerError, "failed to save image")
+			return
+		}
+		req.ImageURL = &imagePath
+	}
+
+	if artistIDStr := r.FormValue("artist_id"); artistIDStr != "" {
+
+		artistID, err := strconv.Atoi(artistIDStr)
+		if err != nil {
+			h.logger.Error("failed to convert artistID string -> int")
+			utilites.RenderError(w, r, http.StatusInternalServerError, "failed to get artist_id")
+			return
+		}
+		req.ArtistID = &artistID
 	}
 
 	if err := h.service.UpdateAlbum(r.Context(), id, req); err != nil {
-		//TODO: err not found
 		h.logger.Error("failed to update album", "error", err)
 		utilites.RenderError(w, r, http.StatusInternalServerError, "failed to update album")
 		return
 	}
 
-	w.WriteHeader(http.StatusNoContent)
+	updatedUlbum, err := h.service.GetAlbumByID(r.Context(), id)
+	if err != nil {
+		h.logger.Error("failed to get updated album", "error", err)
+		utilites.RenderError(w, r, http.StatusInternalServerError, "failed to get updated song")
+		return
+	}
+
+	utilites.RenderJSON(w, r, http.StatusOK, dto.ToResponse(*updatedUlbum))
 }
 
 func (h *Handler) DeleteAlbum(w http.ResponseWriter, r *http.Request) {
@@ -137,7 +194,6 @@ func (h *Handler) DeleteAlbum(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := h.service.DeleteAlbum(r.Context(), id); err != nil {
-		//TODO: err not found
 		h.logger.Error("failed to delete album", "error", err)
 		utilites.RenderError(w, r, http.StatusInternalServerError, "failed to delete album")
 		return
